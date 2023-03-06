874646ec5868526dcd8fc28ce05118ef9b5389d1
mcecula <mc23051988@gmail.com>
2023-03-06 19:29:27 +0100

    zadanie34

---
+++ js/main.js
@@ -0,0 +1,8 @@
+let ujs = 'Uwielbiam JavaScript'
+let jsp = 'Jestem świetnym programistą'
+
+if (ujs === jsp) {
+    console.log('Uwielbiam JavaScript');
+} else {
+    console.log('Jestem świetnym programistą')
+};
