57b705b2920783cab53c6d60b6eac0c4fce63b10
mcecula <mc23051988@gmail.com>
2023-03-09 14:15:56 +0100

    poprawka

--- js/main.js
+++ js/main.js
@@ -1,8 +1,13 @@
-let ujs = 'Uwielbiam JavaScript'
-let jsp = 'Jestem świetnym programistą'
+let str1 = 'Uwielbiam JavaScript';
+let str2 = 'Jestem świetnym programistą';
 
-if (ujs === jsp) {
-    console.log('Uwielbiam JavaScript');
-} else {
-    console.log('Jestem świetnym programistą')
-};
+function str1str2(str1, str2) {
+    if (str1.length > str2.length) {
+        console.log(`${str1} `);
+    }
+    else {
+        console.log(`${str2}`);
+    }
+}
+
+str1str2(str1, str2);
